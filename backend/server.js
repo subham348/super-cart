fa7d9521f55b2686c55874e51c6ce729d7c592f6
subham348 <subhammahato348@gmail.com>
2023-07-31 21:52:09 +0530

    database connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 
+const connectDatabase = require("./config/database");
+
+// Setting path to .env configuration file
 dotenv.config({ path: "backend/config/config.env" });
+
+// Connecting to database
+connectDatabase();
 
 const port = process.env.PORT || 3000;
 
